bc46adf769e2e86a0759dfa629e30681e6d9f91e
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-02-25 09:11:25 +0100

    migliorata gestione input utente

--- exercises.js
+++ exercises.js
@@ -1,41 +1,56 @@
 function calcPrime() {
-    clearPar('result-prime');
-    changeLoaderDisplay('loader1', 'block');
-    const primeWorker = new Worker('./primeWorker.js');
-    primeWorker.addEventListener('message', (event) => {
-        changeLoaderDisplay('loader1', 'none');
-        let primeToNumber = event.data;
-        setMoreValuesPar('result-prime', primeToNumber);
-    });
-    primeWorker.postMessage(getInputNumberValue());
+    const number = getInputNumberValue('inputNumber');
+    if (!isNaN(number)) {
+        clearPar('result-prime');
+        changeLoaderDisplay('loader1', 'block');
+        const primeWorker = new Worker('./primeWorker.js');
+        primeWorker.addEventListener('message', (event) => {
+            changeLoaderDisplay('loader1', 'none');
+            let primeToNumber = event.data;
+            setMoreValuesPar('result-prime', primeToNumber);
+        });
+        primeWorker.postMessage(number);
+    } else {
+        setInputError('result-prime');
+    }
 }
 
 // ------------------------------------------------------------------------- //
 
 function calcFactorial() {
-    clearPar('result-factorial');
-    changeLoaderDisplay('loader2', 'block');
-    const fattorialeWorker = new Worker('./fattorialeWorker.js');
-    fattorialeWorker.addEventListener('message', (event) => {
-        changeLoaderDisplay('loader2', 'none');
-        let fattorialeNumber = event.data;
-        setOneValuesPar('result-factorial', fattorialeNumber);
-    });
-    fattorialeWorker.postMessage(getInputNumberValue());
+    const number = getInputNumberValue('inputNumber');
+    if (!isNaN(number)) {
+        clearPar('result-factorial');
+        changeLoaderDisplay('loader2', 'block');
+        const fattorialeWorker = new Worker('./fattorialeWorker.js');
+        fattorialeWorker.addEventListener('message', (event) => {
+            changeLoaderDisplay('loader2', 'none');
+            let fattorialeNumber = event.data;
+            setOneValuesPar('result-factorial', fattorialeNumber);
+        });
+        fattorialeWorker.postMessage(number);
+    } else {
+        setInputError('result-factorial');
+    }
 }
 
 // ------------------------------------------------------------------------- //
 
 function calcFibonacci() {
-    clearPar('result-fibonacci');
-    changeLoaderDisplay('loader3', 'block');
-    const fibonacciWorker = new Worker('./fibonacciWorker.js');
-    fibonacciWorker.addEventListener('message', (event) => {
-        changeLoaderDisplay('loader3', 'none');
-        let fibonacciToNumber = event.data;
-        setMoreValuesPar('result-fibonacci', fibonacciToNumber);
-    });
-    fibonacciWorker.postMessage(getInputNumberValue());
+    const number = getInputNumberValue('inputNumber');
+    if (!isNaN(number)) {
+        clearPar('result-fibonacci');
+        changeLoaderDisplay('loader3', 'block');
+        const fibonacciWorker = new Worker('./fibonacciWorker.js');
+        fibonacciWorker.addEventListener('message', (event) => {
+            changeLoaderDisplay('loader3', 'none');
+            let fibonacciToNumber = event.data;
+            setMoreValuesPar('result-fibonacci', fibonacciToNumber);
+        });
+        fibonacciWorker.postMessage(number);
+    } else {
+        setInputError('result-fibonacci');
+    }
 }
 
 // ------------------------------------------------------------------------------------------------------------- //
@@ -50,9 +65,14 @@ function changeLoaderDisplay(loaderId, state) {
     loader.style.display = state;
 }
 
-function getInputNumberValue() {
-    const input = document.getElementsByName('inputNumber')[0];
-    return input.value;
+function getInputNumberValue(inputId) {
+    const input = document.getElementById(inputId);
+    if (!isNaN(input.value)) {
+        const inputValue = parseInt(input.value);
+        return inputValue;
+    } else {
+        return NaN;
+    }
 }
 
 function setMoreValuesPar(parId, values) {
@@ -66,5 +86,12 @@ function setMoreValuesPar(parId, values) {
 function setOneValuesPar(parId, text) {
     let par = document.getElementById(parId);
     const node = document.createTextNode(text);
+    par.appendChild(node);
+}
+
+function setInputError(parId) {
+    clearPar(parId);
+    let par = document.getElementById(parId);
+    const node = document.createTextNode('Il valore inserito in input non è un numero, controlla e riprova.');
     par.appendChild(node);
 }
